a8f20264b506810adac49b8d48f50462505a988a
Gustavo <sillesgustavo05@gmail.com>
2022-02-21 15:37:35 -0300

    começo do registro de denuncias

--- sa/CRUD.js
+++ sa/CRUD.js
@@ -1,1 +1,32 @@
-teste
+let denuncias = []
+let obras = []
+
+let problema = document.getElementById("ipt-descricao-denuncia")
+let endereco = document.getElementById("ipt-endereco-denuncia")
+let data = document.getElementById("ipt-dataInicial-denuncia")
+
+
+if(localStorage.length>0){
+    const denunciasExistentes = JSON.parse(localStorage.getItem('denuncias'))
+    for(let i = 0;i<denunciasExistentes.length;i++)
+    {
+        denunciasExistentes.push(denunciasExistentes[i])
+    }
+    
+};
+
+function registrarDenuncias(){
+    denuncias.push({problema: problema.value, endereco: endereco.value,data: data.value.split('-').reverse().join('/')})
+    localStorage.setItem("denuncias", JSON.stringify(denuncias))
+    console.log(localStorage.getItem('denuncias'))
+    console.log("Denuncia feita ( ͡° ͜ʖ ͡°)")
+    Swal.fire({
+        title: 'Parabéns por denunciar! <br> denuncia realizada =',
+        icon: 'success',
+        timer:'700'
+    })
+    problema.value = '';
+    endereco.value = '';
+    data.value= '';
+    
+}
