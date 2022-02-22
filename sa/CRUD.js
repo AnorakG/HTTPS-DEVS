a405df4678f908c043977119a5e26dd37a9ecea7
AnorakG <gabrieltpiber@gmail.com>
2022-02-22 16:19:17 -0300

    imagem com link

--- sa/CRUD.js
+++ sa/CRUD.js
@@ -1,8 +1,6 @@
 let denuncias = []
 let obras = []
 
-let teste = document.getElementById('teste')
-
 let problema = document.getElementById("ipt-descricao-denuncia")
 let endereco = document.getElementById("ipt-endereco-denuncia")
 let data = document.getElementById("ipt-dataInicial-denuncia")
@@ -12,32 +10,12 @@ let descricao = document.getElementById("ipt-descricao")
 let enderecoObra = document.getElementById("ipt-endereco")
 let dataInicial = document.getElementById("ipt-dataInicial")
 let dataFinal = document.getElementById("ipt-dataFinal")
-let imagemDaDenuncia
 
-imagemDenuncia.addEventListener('change', (e)=>{
-    let fileList = e.target.files;
-    let files = {};
-    let index = 0;
-for(let file of fileList) {
-    files[index] = file;
-    index ++;
-}
-
-// Now iterate object instead of array
-Object.keys(files).forEach( key => {
-
-     let reader = new FileReader();
-     
-     reader.addEventListener("load", () =>{  
-        localStorage.setItem('img',reader.result)
-     })
-
-})})
 if(localStorage.length>0){
     const obrasExistentes = JSON.parse(localStorage.getItem('obras'))
     const denunciasExistentes = JSON.parse(localStorage.getItem('denuncias'))
 
-    for(let i = 0;i<obrasExistentes.length;i++)
+    for(let i = 0;i<obrasExistentes;i++)
     {
          obras.push(obrasExistentes[i])
     }
@@ -47,13 +25,14 @@ if(localStorage.length>0){
     }
     
 };
+localStorage.setItem("denuncias",JSON.stringify(denuncias))
+localStorage.setItem("obras",JSON.stringify(obras))
 
 function registrarDenuncias(){
-    imagemDaDenuncia = localStorage.getItem("img")
-    denuncias.push({problema: problema.value, endereco: endereco.value,data: data.value.split('-').reverse().join('/'), imagem: imagemDaDenuncia})
+    denuncias.push({problema: problema.value, endereco: endereco.value,data: data.value.split('-').reverse().join('/'), imagem: imagemDenuncia.value})
     localStorage.setItem("denuncias", JSON.stringify(denuncias))
     console.log(localStorage.getItem('denuncias'))
-    console.log("Denuncia feita ( ͡° ͜ʖ ͡°)")
+    console.log("Denuncia feita")
     Swal.fire({
         title: 'Obrigado pela denunciar! <br> Denúncia realizada ',
         icon: 'success',
@@ -65,7 +44,7 @@ function registrarDenuncias(){
     imagemDenuncia.value = '';
 }
 function registrarObras(){
-    obras.push({descricao: descricao.value, enderecoObra: enderecoObra.value,dataInicial: dataInicial.value.split('-').reverse().join('/'), dataFinal: dataFinal.value.split('-').reverse().join('/')})
+    obras.push({descricao: descricao.value, enderecoObra: enderecoObra.value,dataInicial: dataInicial.value.split('-').reverse().join('/'), dataFinal: dataFinal.value.split('-').reverse().join('/'),imagem: imagemDenuncia.value})
     localStorage.setItem("obras", JSON.stringify(obras))
     console.log(localStorage.getItem('obras'))
     console.log("registro feito ( ͡° ͜ʖ ͡°)")
