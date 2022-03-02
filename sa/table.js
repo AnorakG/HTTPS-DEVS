70b9076e2222ba785d61edb2fbc2802154b645e6
dai222j <rstaffk@gmail.com>
2022-03-02 17:16:52 -0300

    registros

---
+++ sa/table.js
@@ -0,0 +1,78 @@
+
+
+let obras = JSON.parse(localStorage.getItem('obras'))
+function registroObras () {
+    let obras = JSON.parse(localStorage.getItem('obras'))
+    let novoConteudo = document.getElementById("obras")
+    let div = document.createElement("div")
+   
+ 
+    for(let i=0; i<obras.length;i++){
+       const linhaNova = novoConteudo.appendChild(div)
+        for(let j=0; j<5;j++){
+            if(j===0){
+                linhaNova.appendChild(document.createElement("br"))
+                linhaNova.appendChild(document.createTextNode(obras[i].descricao))
+                linhaNova.appendChild(document.createElement("br"))
+            }
+            if(j===1){
+                linhaNova.appendChild(document.createTextNode("Endereço da Obra: "+ obras[i].enderecoObra))
+                linhaNova.appendChild(document.createElement("br"))
+            }
+            if(j===2){
+                linhaNova.appendChild(document.createTextNode("Data Inicial: "+ obras[i].dataInicial))
+                linhaNova.appendChild(document.createElement("br"))
+            }
+            if(j===3){
+                linhaNova.appendChild(document.createTextNode("Data Final: "+ obras[i].dataFinal))
+                linhaNova.appendChild(document.createElement("br"))
+            }
+            if(j===4){
+                let img = document.createElement("img")
+                img.src = obras[i].imagem  
+                img.alt = "Imagem não suportada e/ou incorreta"
+                img.width = 300
+                linhaNova.appendChild(img)
+                linhaNova.appendChild(document.createElement("br"))
+                linhaNova.appendChild(document.createElement("br"))
+            }
+        }
+    }
+}
+
+
+function registroDenuncias () {
+    let denuncias = JSON.parse(localStorage.getItem('denuncias'))
+    let novoRegistro = document.getElementById("denuncias")
+    let div2 = document.createElement("div2") 
+
+
+    
+    for(let i=0; i<denuncias.length;i++){
+        const linhaNova2 = novoRegistro.appendChild(div2)
+         for(let j=0; j<4;j++){
+            if(j===0){
+                 linhaNova2.appendChild(document.createElement("br"))
+                 linhaNova2.appendChild(document.createTextNode(denuncias[i].problema))
+                 linhaNova2.appendChild(document.createElement("br"))
+             }
+            if(j===1){
+                 linhaNova2.appendChild(document.createTextNode("Endereço: "+ denuncias[i].endereco))
+                 linhaNova2.appendChild(document.createElement("br"))
+             }
+            if(j===2){
+                 linhaNova2.appendChild(document.createTextNode("Data: "+ denuncias[i].data))
+                 linhaNova2.appendChild(document.createElement("br"))
+            }
+            if(j===3){
+                let img = document.createElement("img")
+                img.src = denuncias[i].imagem  
+                img.alt = "Imagem não suportada e/ou incorreta"
+                img.width = 300
+                linhaNova2.appendChild(img)
+                linhaNova2.appendChild(document.createElement("br"))
+                linhaNova2.appendChild(document.createElement("br"))
+             }
+        }
+    }
+}
